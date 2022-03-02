611d501ae7a014cdebc091ef080927b20ced4a1c
Alejandro Gonzalez <alejandro.gonzalez4@ct.uneatlantico.es>
2022-03-02 13:28:19 +0100

    test event to add new todo

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,6 +1,9 @@
  <template>
   <div >
     <div>
+        <form data-test="form" @submit.prevent="createTodo">
+            <input data-test="new-todo" v-model="newTodo"/>
+        </form>
         <div v-for="todo in todos" :key="todo.id" data-test="todo">
             {{todo.text}}
         </div>
@@ -13,6 +16,7 @@ export default {
     name: 'TodoApp',
     data(){
         return {
+            newTodo:"",
             todos : [
                 {
                     id: 1,
@@ -32,6 +36,16 @@ export default {
             ] 
         }
     },
+    methods:{
+        createTodo(){
+            this.todos.push({
+                id: 4,
+                text: this.newTodo,
+                completed: false
+            })
+            this.newTodo=""
+        }
+    }
 }
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- tests/unit/TodoApp.spec.js
+++ tests/unit/TodoApp.spec.js
@@ -1,5 +1,6 @@
 import { mount } from "@vue/test-utils";
 import TodoApp from '@/components/TodoApp.vue'
+import { assert } from "@vue/compiler-core";
 
 describe("TodoApp.vue", () => {
     it("Should render todo text", () => {
@@ -8,5 +9,16 @@ describe("TodoApp.vue", () => {
         const todo = wrapper.get('[data-test="todo"]')
 
         expect(todo.text()).toBe("Learn Vue Testing")
+    })
+
+    it("should add new todo", async () => {
+        const wrapper = mount(TodoApp)
+
+        expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(3);
+        
+        await wrapper.get('[data-test="new-todo"]').setValue("New todo")
+        await wrapper.get('[data-test="form"]').trigger('submit')
+
+        expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(4);
     })
 })
