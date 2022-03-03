256f328f217fc7b9ee85d0310c5341e3fde41ac7
Alejandro Gonzalez <alejandro.gonzalez4@ct.uneatlantico.es>
2022-03-03 12:33:30 +0100

    mounting app before running test

--- tests/unit/TodoApp.spec.js
+++ tests/unit/TodoApp.spec.js
@@ -2,31 +2,27 @@ import { mount } from "@vue/test-utils";
 import TodoApp from '@/components/TodoApp.vue'
 
 describe("TodoApp.vue", () => {
+    
+    let wrapper
+
+    beforeEach(()=> {
+        wrapper = mount(TodoApp)
+    })
 
     it("Should render todo text", () => {
-        const wrapper = mount(TodoApp)
-
         const todo = wrapper.get('[data-test="todo"]')
-
         expect(todo.text()).toBe("Learn Vue Testing")
     })
 
     it("should add new todo", async () => {
-        const wrapper = mount(TodoApp)
-
         expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(3);
-        
         await wrapper.get('[data-test="new-todo"]').setValue("New todo")
         await wrapper.get('[data-test="form"]').trigger('submit')
-
         expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(4);
     })
 
     it("should change value checkbox", async () => {
-        const wrapper = mount(TodoApp)
-
         await wrapper.get('[data-test="todo-checkbox"]').setValue(true)
-
         expect(wrapper.get('[data-test="todo"]').classes()).toContain('completed')
     })
 })
